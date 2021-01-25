dbbbe3c5cf5c20b12ecfa5ec06efa30b991862ed
anthonyVladimir26 <77985935+anthonyVladimir26@users.noreply.github.com>
2021-01-25 12:34:38 -0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/UsuarioSQLHelper.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class UsuarioSQLHelper extends SQLiteOpenHelper {
+
+    //creamos tabala
+
+    String sql ="CREATE TABLE cliente(id INTEGER, nombre TEXT, apellidos TEXT)";
+    public UsuarioSQLHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
